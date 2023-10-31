10cd32f52303a84d676fb99c3b6a731c1ec892fb
Valentina De Mite <valesakura97@gmail.com>
2023-10-31 16:17:02 +0100

    fixed arrays

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
+use App\Http\Requests\StoreComicRequest;
+use App\Models\Comic;
+
 
 class ComicsController extends Controller
 {
-    //
+    public function index()
+    {
+        // return dd(Comic::all());
+        $comics_list = Comic::all();
+        return view('admin.comics.index', compact('comics_list'));
+    }
+
+    /**
+     * Show the form for a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.comics.create');
+    }
+
+    
 }
--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -23,8 +23,8 @@ class ComicsTableSeeder extends Seeder
             $new_comic->price = $comic['price'];
             $new_comic->series = $comic['series'];
             $new_comic->sale_date = $comic['sale_date'];
-            $new_comic->artists = $comic['artists'];
-            $new_comic->writers = $comic['writers'];
+            $new_comic->artists = implode(',',$comic['artists']);
+            $new_comic->writers = implode(',',$comic['writers']);
             $new_comic->save();
         }
     }
