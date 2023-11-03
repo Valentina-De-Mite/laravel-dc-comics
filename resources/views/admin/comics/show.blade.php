5f90b500696d6cd0e295730e9d55bbb66db5f29d
Valentina De Mite <valesakura97@gmail.com>
2023-11-03 17:14:53 +0100

    added description and fixed layout

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -2,38 +2,39 @@
 
 @section('content')
 
-
-<div class="banner-tot  p-5 mb-4 bg-dark text-white rounded-0">
-    <div class="container-fluid">
-        <h1 class="display-5 fw-bold">{{$comic->title}}</h1>
-        <p>
-           {{$comic->description}}
-        </p>
+<div class="jumbotron-tot">
+    <div class=" p-5  bg-dark text-white rounded-0">
+        <div class="container ">
+            <h2 class=" fw-bold mb-3">{{$comic->title}}</h2>
+            <p class="mt-4 fs-4">
+            {{$comic->description}}
+            </p>
+            <p class="fs-5 mb-3">Artist: {{$comic->artists}};</p>
+            <p class="fs-5">Writers: {{$comic->writers}};</p>
+        </div>
     </div>
-</div>
+<div class="jumbotron-tot py-5">
+    <div class="container d-flex  ">
 
-<div class="container d-flex my-5">
-
-    <div class="col-8">
+    <div class="col-7">
 
     <img width="w-75" class="img-fluid shadow rounded" src="{{$comic->thumb}}" alt="">
 </div>
     
-    <div class="d-flex col-4 ">
-<div class="description align-self-center">
-        <strong class="text-muted fs-2">Description:</strong>
-        <div class="fs-5"> Price: {{$comic->price}};</div>
-        <div class="fs-5">Series: {{$comic->series}};</div>
-        
+    <div class="d-flex col-4">
+        <div class="description align-self-center  bg-dark-subtle p-5 rounded-2">
+            <strong class="text-muted fs-2">Description:</strong>
+            <div class="fs-5"> Price: {{$comic->price}};</div>
+            <div class="fs-5">Series: {{$comic->series}};</div>
+            
 
-        <a class="btn btn-success mt-4" href="#" role="button">Buy Now</a>
+            <a class="btn btn-success mt-4 p-2 big m-auto" href="#" role="button">Buy Now</a>
+        </div>
     </div>
 </div>
-    
 
 
 
 
-</div>
 
 @endsection
