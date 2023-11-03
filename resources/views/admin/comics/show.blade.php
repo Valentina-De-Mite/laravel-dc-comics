93a2556d9af266461b74ae700e3c026bb9d577a5
Valentina De Mite <valesakura97@gmail.com>
2023-11-03 16:51:33 +0100

    fixed show view

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 
-<div class="p-5 mb-4 bg-dark text-white rounded-0">
-    <div class="container-fluid py-5">
+
+<div class="banner-tot  p-5 mb-4 bg-dark text-white rounded-0">
+    <div class="container-fluid">
         <h1 class="display-5 fw-bold">{{$comic->title}}</h1>
         <p>
            {{$comic->description}}
@@ -11,19 +12,24 @@
     </div>
 </div>
 
-<div class="container d-flex gap-2">
+<div class="container d-flex my-5">
 
-    <img width="600" class="img-fluid shadow-lg" src="{{$comic->thumb}}" alt="">
+    <div class="col-8">
 
-    <div class="text">
-        <strong class="text-muted">Description</strong>
-        <div class="display-3"> {{$comic->price}}</div>
-        <div class="display-3"> {{$comic->series}}</div>
-        <div class="display-3"> {{$comic->artists}}</div>
-        <div class="display-3"> {{$comic->writers}}</div>
+    <img width="w-75" class="img-fluid shadow rounded" src="{{$comic->thumb}}" alt="">
+</div>
+    
+    <div class="d-flex col-4 ">
+<div class="description align-self-center">
+        <strong class="text-muted fs-2">Description:</strong>
+        <div class="fs-5"> Price: {{$comic->price}};</div>
+        <div class="fs-5">Series: {{$comic->series}};</div>
+        
 
         <a class="btn btn-success mt-4" href="#" role="button">Buy Now</a>
     </div>
+</div>
+    
 
 
 
